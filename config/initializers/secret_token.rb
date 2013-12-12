6a6683e80ee355c6a86e9a50b032c2a40a0869e9
UnGun <shie19851018@gmail.com>
2013-12-12 10:57:09 +0800

    first  rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greenworldhotels::Application.config.secret_key_base = '681498b21955de0a8f74efb8eab16d6041676f5355acd5357f071b976897a033ccffa454bf311ffd222ebe2fe6fa7e9d4fbe08cbed0c251b001f98f144c3ced4'
